3f50d3f07f5b3ced8201d17e283f024faecdef37
Robert Balonek <robertbalonek@gmail.com>
2022-10-15 14:31:54 -0700

    Password Eye

--- src/components/Login/PasswordLogin/PasswordLogin.jsx
+++ src/components/Login/PasswordLogin/PasswordLogin.jsx
@@ -6,13 +6,15 @@ export default function PasswordLogin() {
   const [themeColor, setThemeColor] = useState("light")
   const [colorScheme, setColorScheme] = useState("password-login__card card-light")
   const [buttonColor, setButtonColor] = useState("btn__password-login btn-dark")
+  const [seePassword, setSeePassword] = useState('password')
+  const [isVisImg, SetIsVisImg] = useState("https://res.cloudinary.com/bobalobbadingdong/image/upload/v1665867900/Global%20Images/Eye_cfm479.png")
 
   useEffect(() => {
     if (themeColor === 'light') {
-      setButtonColor("btn__password-login btn-dark")
+      setButtonColor("btn__password-login btn-light")
       setColorScheme("password-login__card card-light")
     }else if(themeColor === 'dark') {
-      setButtonColor("btn__password-login btn-light")
+      setButtonColor("btn__password-login btn-dark")
       setColorScheme("password-login__card card-dark")
     }else if(themeColor === 'blue') {
       setButtonColor("btn__password-login btn-blue")
@@ -36,6 +38,16 @@ export default function PasswordLogin() {
     setThemeColor('orange')
   }
 
+  const passwordIsVis = () => {
+    if (isVisImg === "https://res.cloudinary.com/bobalobbadingdong/image/upload/v1665867900/Global%20Images/Eye_cfm479.png") {
+      SetIsVisImg("https://res.cloudinary.com/bobalobbadingdong/image/upload/v1665867473/Global%20Images/Eye_mffqal.png")
+      setSeePassword('text')
+    } else {
+      SetIsVisImg("https://res.cloudinary.com/bobalobbadingdong/image/upload/v1665867900/Global%20Images/Eye_cfm479.png")
+      setSeePassword('password')
+    }
+  }
+
   return (
     <div className='password-login__container'>
       <div className={colorScheme}>
@@ -50,8 +62,11 @@ export default function PasswordLogin() {
             </div>
             <form style={{display:'flex', flexDirection:'column'}}>
             <label style={{fontWeight:'600', fontSize:'x-small', marginBottom:'5px'}}>Password</label>
-            <input className="password__card-input" type="text" id="fname" name="fname"></input>
-            </form>
+            <div style={{width:'100%', position:'relative', bottom:'10px'}}>
+            <input type={seePassword} className="password__card-input" id="fname" name="fname"></input>
+            <img onClick={passwordIsVis} style={{height:'20px',width:'20px', position:'relative', top:'5px', backgroundColor:'white', borderRadius:'50%', cursor:'pointer'}} alt="isVis" src={isVisImg}/> 
+            </div>
+          </form>
         </div>
         
         <div className='password__card-btn-holder'>
